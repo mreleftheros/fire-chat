730045ec937be67b478b8a57f5a99d3e70e51741
eleftheros <eleftheros_all@protonmail.com>
2021-09-02 20:32:28 +0300

    Add hamburger transition and function to toggle it

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
 import { checkAuthentication } from "./firebase";
 
-checkAuthentication();
+// variables
+const hamburger = document.getElementById("hamburger");
+
+// function that toggles class open to menu and hamburger
+const toggleMenu = e => {
+  const menu = document.getElementById("menu");
+
+  e.currentTarget.classList.toggle("open");
+  menu.classList.toggle("open");
+};
+
+hamburger.addEventListener("click", toggleMenu);
