e20f4fdb3beff6ce0c260391e415854a2de00965
eleftheros <eleftheros_all@protonmail.com>
2021-09-04 13:34:19 +0300

    Create chat.js and add class Chat

---
+++ src/js/chat.js
@@ -0,0 +1,12 @@
+class Chat {
+  constructor(name, chatroom, message, timestamp) {
+    this.name = name;
+    this.chatroom = chatroom;
+    this.message = message;
+    this.timestamp =timestamp;
+  }
+
+
+}
+
+export {Chat as default};
